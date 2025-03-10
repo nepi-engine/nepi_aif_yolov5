75f17d1671dac0ea7859c7967589bd3b98d3800b
Jason Seawall <jseawall@numurus.com>
2025-03-10 09:59:42 -0700

    AI Detector updates

--- scripts/nepi_ai_yolov5_detection_node.py
+++ scripts/nepi_ai_yolov5_detection_node.py
@@ -93,8 +93,8 @@ class Yolov5Detector():
                     model_type = model_info['type']['name']
                     model_description = model_info['description']['name']
                     self.classes = model_info['classes']['names']
-                    self.img_width = model_info['image_size']['image_width']['value']
-                    self.img_height = model_info['image_size']['image_height']['value']
+                    self.proc_img_width = model_info['image_size']['image_width']['value']
+                    self.proc_img_height = model_info['image_size']['image_height']['value']
                 except Exception as e:
                     nepi_msg.publishMsgWarn(self,"Failed to get required model info from params: " + str(e))
                     rospy.signal_shutdown("Failed to get valid model file paths")
@@ -117,12 +117,14 @@ class Yolov5Detector():
                 self.ai_if = AiDetectorIF(model_name = self.node_name,
                                     framework = model_framework,
                                     description = model_description,
-                                    img_height = self.img_height,
-                                    img_width = self.img_width,
+                                    proc_img_height = self.proc_img_height,
+                                    proc_img_width = self.proc_img_width,
                                     classes_list = self.classes,
                                     defualt_config_dict = self.defualt_config_dict,
                                     all_namespace = self.all_namespace,
-                                    processDetectionFunction = self.processDetection)
+                                    preprocessImageFunction = self.preprocessImage,
+                                    processDetectionFunction = self.processDetection,
+                                    has_img_tiling = False)
 
                 #########################################################
                 ## Initiation Complete
@@ -133,63 +135,90 @@ class Yolov5Detector():
               
 
 
-
-    def processDetection(self,cv2_img, threshold):
-        start_time = time.time()
-        detect_dict_list = [TEST_DETECTION_DICT_ENTRY]
-        # Example image
-        #img = 'https://ultralytics.com/images/zidane.jpg'
+    def preprocessImage(self,cv2_img,options_dict):
+        height, width = cv2_img.shape[:2]
+        
+        # For Future
+        '''
+        tile = False
+        if 'tile'  in options_dict.keys():
+            tile = options_dict['tile']
+        '''
+        
         # Convert BW image to RGB
         if cv2_img.shape[2] != 3:
             cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_GRAY2BGR)
-        # Convert BGR image to RGB image
-        cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-        cv2_img_shape = cv2_img.shape
-        cv2_img_width = cv2_img_shape[1]
-        cv2_img_height = cv2_img_shape[0]
-        cv2_img_area = cv2_img_shape[0] * cv2_img_shape[1]
-        #nepi_msg.publishMsgInfo(self,"Original image size: " + str(orig_size))
 
-        # Update model settings
-        self.model.conf = threshold  # Confidence threshold (0-1)
-        self.model.iou = 0.45  # NMS IoU threshold (0-1)
-        self.model.max_det = 20  # Maximum number of detections per image
-        #self.model.eval()
-        # Run the detection model on tensor
+        #nepi_msg.publishMsgInfo(self,":yolov5: Preprocessed image with image size: " + str(cv2_img.shape))
+        # Create image dict with new image
+        img_dict = dict()
+        img_dict['cv2_img'] = cv2_img
+        img_shape = cv2_img.shape
+        img_dict['org_width'] = width 
+        img_dict['orig_height'] = height 
+        img_dict['tiling'] = False
 
-        try:
-            # Inference
-            results = self.model(cv2_img)
-            #nepi_msg.publishMsgInfo(self,"Got Yolo detection result: " + str(results))
-            rp = results.pandas().xyxy[0]  # img1 predictions (pandas)
-            #nepi_msg.publishMsgInfo(self,"Got pandas formated results: " + str(rp))
-        except Exception as e:
-            nepi_msg.publishMsgInfo(self,"Failed to process img with exception: " + str(e))
-        detect_time = round( (time.time() - start_time) , 3)
-        #nepi_msg.publishMsgInfo(self,"Detect Time: {:.2f}".format(detect_time))
+        return img_dict
+
+
+
+    def processDetection(self,img_dict, threshold):
         detect_dict_list = []
-        for i, name in enumerate(rp['name']):
-            detect_box_area = ( int(rp['xmax'][i]) - int(rp['xmin'][i]) ) * ( int(rp['ymax'][i]) - int(rp['ymin'][i]) )
-            detect_box_ratio = detect_box_area / cv2_img_area
-            detect_dict = {
-                'name': rp['name'][i], # Class String Name
-                'id': rp['class'][i], # Class Index from Classes List
-                'uid': '', # Reserved for unique tracking by downstream applications
-                'prob': rp['confidence'][i], # Probability of detection
-                'xmin': int(rp['xmin'][i]),
-                'ymin': int(rp['ymin'][i]),
-                'xmax': int(rp['xmax'][i]),
-                'ymax': int(rp['ymax'][i]),
-                'width_pixels': cv2_img_width,
-                'height_pixels': cv2_img_height,
-                'area_pixels': detect_box_area,
-                'area_ratio': detect_box_ratio
-            }
-            detect_dict_list.append(detect_dict)
-            #nepi_msg.publishMsgInfo(self,"Got detect dict entry: " + str(detect_dict))
-        detect_time = round( (time.time() - start_time) , 3)
-        #nepi_msg.publishMsgInfo(self,"Detect Time: {:.2f}".format(detect_time))
-        return detect_dict_list, detect_time
+        tile = False
+        # For Future
+        #if 'tile'  in options_dict.keys():
+        #    tile = options_dict['tile']
+        if img_dict is not None:
+            if 'cv2_img' in img_dict.keys():
+                cv2_img = img_dict['cv2_img']
+                if cv2_img is not None:
+
+                    # Convert BGR image RGB
+                    cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+                    
+                    cv2_img = img_dict['cv2_img']
+                    cv2_img_shape = cv2_img.shape
+                    cv2_img_width = cv2_img_shape[1]
+                    cv2_img_height = cv2_img_shape[0]
+                    cv2_img_area = cv2_img_shape[0] * cv2_img_shape[1]
+                    #nepi_msg.publishMsgInfo(self,":yolov5: Processing detection with image size: " + str(cv2_img_shape))
+
+                    # Update model settings
+                    self.model.conf = threshold  # Confidence threshold (0-1)
+                    self.model.iou = 0.45  # NMS IoU threshold (0-1)
+                    self.model.max_det = 20  # Maximum number of detections per image
+                    #self.model.eval()
+                    # Run the detection model on tensor
+
+                    try:
+                        # Inference
+                        results = self.model(cv2_img)
+                        #nepi_msg.publishMsgInfo(self,"Got Yolo detection result: " + str(results))
+                        rp = results.pandas().xyxy[0]  # img1 predictions (pandas)
+                        #nepi_msg.publishMsgInfo(self,"Got pandas formated results: " + str(rp))
+                    except Exception as e:
+                        nepi_msg.publishMsgInfo(self,"Failed to process img with exception: " + str(e))
+                    detect_dict_list = []
+                    for i, name in enumerate(rp['name']):
+                        detect_box_area = ( int(rp['xmax'][i]) - int(rp['xmin'][i]) ) * ( int(rp['ymax'][i]) - int(rp['ymin'][i]) )
+                        detect_box_ratio = detect_box_area / cv2_img_area
+                        detect_dict = {
+                            'name': rp['name'][i], # Class String Name
+                            'id': rp['class'][i], # Class Index from Classes List
+                            'uid': '', # Reserved for unique tracking by downstream applications
+                            'prob': rp['confidence'][i], # Probability of detection
+                            'xmin': int(rp['xmin'][i]),
+                            'ymin': int(rp['ymin'][i]),
+                            'xmax': int(rp['xmax'][i]),
+                            'ymax': int(rp['ymax'][i]),
+                            'width_pixels': cv2_img_width,
+                            'height_pixels': cv2_img_height,
+                            'area_pixels': detect_box_area,
+                            'area_ratio': detect_box_ratio
+                        }
+                        detect_dict_list.append(detect_dict)
+                        #nepi_msg.publishMsgInfo(self,"Got detect dict entry: " + str(detect_dict))
+        return detect_dict_list
 
 
 
