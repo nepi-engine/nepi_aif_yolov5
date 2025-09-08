334b68652cd42ebdc69599566b9a286c2bc65f47
Jason Seawall <jseawall@numurus.com>
2025-09-08 16:51:46 -0700

    **

--- scripts/nepi_ai_yolov5_detection_node.py
+++ scripts/nepi_ai_yolov5_detection_node.py
@@ -124,8 +124,8 @@ class Yolov5Detector():
                 # Initialize Detector with Blank Img
                 self.msg_if.pub_info("Initializing detector with blank img")
                 init_cv2_img=nepi_img.create_cv2_blank_img()
-                init_cv2_img=self.preprocessImage(init_cv2_img)
-                det_dict=self.processDetection(init_cv2_img)
+                img_dict=self.preprocessImage(init_cv2_img)
+                det_dict=self.processDetection(img_dict)
 
 
                 self.msg_if.pub_info("Starting ai_if with default_config_dict: " + str(self.default_config_dict))
@@ -152,7 +152,7 @@ class Yolov5Detector():
               
 
 
-    def preprocessImage(self,cv2_img,options_dict):
+    def preprocessImage(self,cv2_img,options_dict=dict()):
         height, width = cv2_img.shape[:2]
         
         # For Future
@@ -183,7 +183,7 @@ class Yolov5Detector():
 
 
 
-    def processDetection(self,img_dict, threshold):
+    def processDetection(self,img_dict, threshold = 0.3):
         detect_dict_list = []
 
         tile = False
