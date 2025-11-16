22c5a4cec059c178d6921b303162462c0c4e06fe
Jason Seawall <jseawall@numurus.com>
2025-11-16 11:29:38 -0800

    **

--- scripts/nepi_ai_yolov5_detection_node.py
+++ scripts/nepi_ai_yolov5_detection_node.py
@@ -117,17 +117,26 @@ class Yolov5Detector():
 
                 self.classes = model_info['classes']['names']
 
-                
+                self.msg_if.pub_warn("Loading model: " + self.node_name)
                 raw_yolov5_path = r"{}".format(self.yolov5_path)
                 self.model = torch.hub.load(raw_yolov5_path,'custom', path=self.weight_file_path,source='local')
                 
                 # Initialize Detector with Blank Img
-                self.msg_if.pub_info("Initializing detector with blank img")
+                self.msg_if.pub_warn("Initializing detector with blank img")
                 init_cv2_img=nepi_img.create_cv2_blank_img()
-                img_dict=self.preprocessImage(init_cv2_img)
-                det_dict=self.processDetection(img_dict)
+                det_dict=self.processDetection(init_cv2_img)
 
+                # Run Tests
+                NUM_TESTS=10
+                self.msg_if.pub_warn("Running Detection Speed Test on " + str(NUM_TESTS) + " Images")
+                start_time = time.time()
+                for i in range(1, NUM_TESTS):
+                    det_dict=self.processDetection(init_cv2_img)
+                elapsed_time = ( time.time() - start_time )  / 3 # Slower for real images
+                detect_rate = round( float(1.0)/elapsed_time * NUM_TESTS , 2)
+                self.msg_if.pub_warn("Average Detection Rate: " + str(detect_rate) + " hz")
 
+                # Create API IF Class
                 self.msg_if.pub_info("Starting ai_if with default_config_dict: " + str(self.default_config_dict))
                 self.ai_if = AiDetectorIF(
                                     namespace = self.node_namespace,
@@ -139,7 +148,6 @@ class Yolov5Detector():
                                     classes_list = self.classes,
                                     default_config_dict = self.default_config_dict,
                                     all_namespace = self.all_namespace,
-                                    preprocessImageFunction = self.preprocessImage,
                                     processDetectionFunction = self.processDetection,
                                     has_img_tiling = False)
 
@@ -152,99 +160,88 @@ class Yolov5Detector():
               
 
 
-    def preprocessImage(self,cv2_img,options_dict=dict()):
-        height, width = cv2_img.shape[:2]
-        
-        # For Future
-        '''
-        tile = False
-        if 'tile'  in options_dict.keys():
-            tile = options_dict['tile']
-        '''
-        [cv2_img,ratio,new_width,new_height] = nepi_img.resize_proportionally(cv2_img, self.proc_img_width,self.proc_img_height,interp = cv2.INTER_NEAREST)
-        
-        # Convert BW image to RGB
-        if nepi_img.is_gray(cv2_img):
-            cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_GRAY2BGR)
+    def processDetection(self, cv2_img, img_dict=dict(), threshold = 0.3, resize = False, verbose = False):
 
-        #self.msg_if.pub_info(":yolov5: Preprocessed image with image size: " + str(cv2_img.shape))
-        # Create image dict with new image
-        img_dict = dict()
-        img_dict['cv2_img'] = cv2_img
-        img_shape = cv2_img.shape
-        img_dict['image_width'] = width 
-        img_dict['image_height'] = height 
-        img_dict['prc_width'] = new_width 
-        img_dict['prc_height'] = new_height 
-        img_dict['ratio'] = ratio 
+        img_dict['image_width'] = 1
+        img_dict['image_height'] = 1 
+        img_dict['prc_width'] = 1
+        img_dict['prc_height'] = 1 
+        img_dict['ratio'] = 1
         img_dict['tiling'] = False
 
-        return img_dict
-
-
-
-    def processDetection(self,img_dict, threshold = 0.3):
         detect_dict_list = []
+        if cv2_img is not None:
 
-        tile = False
-        # For Future
-        #if 'tile'  in options_dict.keys():
-        #    tile = options_dict['tile']
-        if img_dict is not None and self.model is not None:
-            if 'cv2_img' in img_dict.keys():
-                cv2_img = img_dict['cv2_img']
-                if cv2_img is not None:
+                cv2_img_shape = cv2_img.shape
+                cv2_img_width = cv2_img_shape[1]
+                cv2_img_height = cv2_img_shape[0]
+                cv2_img_area = cv2_img_shape[0] * cv2_img_shape[1]
 
-                    # Convert BGR image RGB
+                if resize == True:
+                    [cv2_img,rescale_ratio,prc_width,prc_height] = nepi_img.resize_proportionally(cv2_img, self.proc_img_width,self.proc_img_height,interp = cv2.INTER_NEAREST)
+                else:
+                    rescale_ratio = 1
+                    prc_width = cv2_img_width
+                    prc_height = cv2_img_height
+
+                # Convert to RGB
+                if nepi_img.is_gray(cv2_img):
+                    cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_GRAY2BGR)
+                else:
                     cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-                    cv2_img_shape = cv2_img.shape
-                    cv2_img_width = cv2_img_shape[1]
-                    cv2_img_height = cv2_img_shape[0]
-                    cv2_img_area = cv2_img_shape[0] * cv2_img_shape[1]
-                    #self.msg_if.pub_info(":yolov5: Processing detection with image size: " + str(cv2_img_shape))
 
-                    # Update model settings
-                    self.model.conf = threshold  # Confidence threshold (0-1)
-                    self.model.iou = 0.45  # NMS IoU threshold (0-1)
-                    self.model.max_det = 20  # Maximum number of detections per image
-                    #self.model.eval()
-                    # Run the detection model on tensor
+                #self.msg_if.pub_info(":yolov5: Preprocessed image with image size: " + str(cv2_img.shape))
+                # Create image dict with new image
+                img_dict['image_width'] = cv2_img_width 
+                img_dict['image_height'] = cv2_img_height 
+                img_dict['prc_width'] = prc_width 
+                img_dict['prc_height'] = prc_height 
+                img_dict['ratio'] = rescale_ratio 
+                img_dict['tiling'] = False
 
-                    try:
-                        # Inference
-                        results = self.model(cv2_img)
-                        #self.msg_if.pub_info("Got Yolo detection result: " + str(results))
-                        rp = results.pandas().xyxy[0]  # img1 predictions (pandas)
-                        #self.msg_if.pub_info("Got pandas formated results: " + str(rp))
-                    except Exception as e:
-                        self.msg_if.pub_info("Failed to process img with exception: " + str(e))
-                    detect_dict_list = []
-                    rescale_ratio = float(1) / img_dict['ratio']
-                    for i, name in enumerate(rp['name']):
-                        detect_box_area = ( int(rp['xmax'][i]) - int(rp['xmin'][i]) ) * ( int(rp['ymax'][i]) - int(rp['ymin'][i]) )
-                        detect_box_ratio = detect_box_area / cv2_img_area
-                        detect_dict = {
-                            'name': rp['name'][i], # Class String Name
-                            'id': rp['class'][i], # Class Index from Classes List
-                            'uid': '', # Reserved for unique tracking by downstream applications
-                            'prob': rp['confidence'][i], # Probability of detection
-                            'xmin': int(rp['xmin'][i]),
-                            'ymin': int(rp['ymin'][i]),
-                            'xmax': int(rp['xmax'][i]),
-                            'ymax': int(rp['ymax'][i]),
-                            'area_pixels': detect_box_area,
-                            'area_ratio': detect_box_ratio
-                        }
-                        # Rescale to orig image size
-                        detect_dict['xmin'] = int(detect_dict['xmin'] * rescale_ratio)
-                        detect_dict['ymin'] = int(detect_dict['ymin'] * rescale_ratio)
-                        detect_dict['xmax'] = int(detect_dict['xmax'] * rescale_ratio)
-                        detect_dict['ymax'] = int(detect_dict['ymax'] * rescale_ratio)
-                        detect_dict_list.append(detect_dict)
-                        #self.msg_if.pub_info("Got detect dict entry: " + str(detect_dict))
-        else:
-            time.sleep(1)
-        return detect_dict_list
+
+
+                # Update model settings
+                self.model.conf = threshold  # Confidence threshold (0-1)
+                self.model.iou = 0.45  # NMS IoU threshold (0-1)
+                self.model.max_det = 20  # Maximum number of detections per image
+                #self.model.eval()
+                # Run the detection model on tensor
+
+                try:
+                    # Inference
+                    results = self.model(cv2_img)
+                    #self.msg_if.pub_info("Got Yolo detection result: " + str(results))
+                    rp = results.pandas().xyxy[0]  # img1 predictions (pandas)
+                    #self.msg_if.pub_info("Got pandas formated results: " + str(rp))
+                except Exception as e:
+                    self.msg_if.pub_info("Failed to process img with exception: " + str(e))
+                detect_dict_list = []
+                rescale_ratio = float(1) / img_dict['ratio']
+                for i, name in enumerate(rp['name']):
+                    detect_box_area = ( int(rp['xmax'][i]) - int(rp['xmin'][i]) ) * ( int(rp['ymax'][i]) - int(rp['ymin'][i]) )
+                    detect_box_ratio = detect_box_area / cv2_img_area
+                    detect_dict = {
+                        'name': rp['name'][i], # Class String Name
+                        'id': rp['class'][i], # Class Index from Classes List
+                        'uid': '', # Reserved for unique tracking by downstream applications
+                        'prob': rp['confidence'][i], # Probability of detection
+                        'xmin': int(rp['xmin'][i]),
+                        'ymin': int(rp['ymin'][i]),
+                        'xmax': int(rp['xmax'][i]),
+                        'ymax': int(rp['ymax'][i]),
+                        'area_pixels': detect_box_area,
+                        'area_ratio': detect_box_ratio
+                    }
+                    # Rescale to orig image size
+                    detect_dict['xmin'] = int(detect_dict['xmin'] * rescale_ratio)
+                    detect_dict['ymin'] = int(detect_dict['ymin'] * rescale_ratio)
+                    detect_dict['xmax'] = int(detect_dict['xmax'] * rescale_ratio)
+                    detect_dict['ymax'] = int(detect_dict['ymax'] * rescale_ratio)
+                    detect_dict_list.append(detect_dict)
+                    #self.msg_if.pub_info("Got detect dict entry: " + str(detect_dict))
+                    
+        return [detect_dict_list, img_dict]
 
 
 
